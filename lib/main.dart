94fd2be960a405feb0f34b81fe9f6b1f08d7f312
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-18 12:26:31 +0200

    add favorite page -- end of tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -139,6 +139,27 @@ class GeneratorPage extends StatelessWidget {
         ],
       ),
     ]));
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Text('Your favorites: '),
+        for (var pair in appState.favorites)
+          ListTile(leading: Icon(Icons.favorite), title: Text(pair.asLowerCase))
+      ],
+    );
   }
 }
 
